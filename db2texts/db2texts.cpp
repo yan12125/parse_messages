8b11a7a716433dcdf01e1d49d9fee2828d88249d
Yen Chi Hsuan <yan12125@gmail.com>
2014-12-16 23:59:21 +0800

    Build succeed on ubuntu 14.10

--- db2texts/db2texts.cpp
+++ db2texts/db2texts.cpp
@@ -10,18 +10,33 @@
 #include <QDir>
 using namespace std;
 
-int main(int argc, char* argv[])
+void writeThread(const QDir& output_dir, const string& a_thread, int gmt_offset, sqlite::connection& con)
 {
-    if(argc != 2)
-    {
-        return -1;
-    }
     locale loc(locale::classic(), new boost::posix_time::time_facet("%Y/%m/%d %H:%M")); // seems Facebook does not record seconds info
     stringstream ss;
     ss.imbue(loc);
 
+    QString filename = output_dir.filePath(QString::fromStdString(a_thread+".txt"));
+    ofstream f(filename.toLocal8Bit().constData());
+
+    sqlite::query getMessages(con, "SELECT timestamp,contentIndex,user,content FROM messages WHERE thread=? ORDER BY timestamp ASC, contentIndex DESC"); // facebook put newer messages first
+    getMessages.bind(1, a_thread);
+    boost::shared_ptr<sqlite::result> result2 = getMessages.get_result();
+    while(result2->next_row())
+    {
+        int timestamp = result2->get_int(0);
+        string user = result2->get_string(2);
+        string content = result2->get_string(3);
+        ss << boost::posix_time::from_time_t(timestamp + gmt_offset * 3600);
+        f << ss.str() << "," << user << "," << content << "\n";
+        ss.str(string());
+    }
+    f.close();
+}
+
+void iterateThreads(sqlite::connection& con, const QDir& output_dir)
+{
     vector<string> threads;
-    sqlite::connection con(QString::fromLocal8Bit(argv[1]).toStdString());
     sqlite::query threadQuery(con, "SELECT DISTINCT thread FROM messages");
     sqlite::result_type threadResults = threadQuery.get_result();
     while(threadResults->next_row())
@@ -35,26 +50,24 @@ int main(int argc, char* argv[])
     gmtResult->next_row();
     int gmt_offset = stoi(gmtResult->get_string(0));
 
-    QDir().mkdir("output");
-    QDir output_dir("output");
-    ofstream f;
     for(string a_thread : threads)
     {
-        QString filename = output_dir.filePath(QString::fromStdString(a_thread+".txt"));
-        f.open(filename.toLocal8Bit().constData());
-        sqlite::query getMessages(con, "SELECT timestamp,contentIndex,user,content FROM messages WHERE thread=? ORDER BY timestamp ASC, contentIndex DESC"); // facebook put newer messages first
-        getMessages.bind(1, a_thread);
-        boost::shared_ptr<sqlite::result> result2 = getMessages.get_result();
-        while(result2->next_row())
-        {
-            int timestamp = result2->get_int(0);
-            string user = result2->get_string(2);
-            string content = result2->get_string(3);
-            ss << boost::posix_time::from_time_t(timestamp + gmt_offset * 3600);
-            f << ss.str() << "," << user << "," << content << "\n";
-            ss.str(string());
-        }
-        f.close();
+        writeThread(output_dir, a_thread, gmt_offset, con);
     }
+}
+
+int main(int argc, char* argv[])
+{
+    if(argc != 2)
+    {
+        return -1;
+    }
+
+    QDir().mkdir("output");
+    QDir output_dir("output");
+
+    sqlite::connection con(QString::fromLocal8Bit(argv[1]).toStdString());
+    iterateThreads(con, output_dir);
+
     return 0;
 }
